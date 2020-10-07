cdda3c47030bb31321cfbb08438ada093e868efb
Michael Singhurse <michaelsinghurse@gmail.com>
2020-10-07 10:44:49 -0500

    added morgan for http logging

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 "use strict";
 
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(morgan("dev"));
 app.use(express.static("client"));
 
 app.listen(port, () => {
