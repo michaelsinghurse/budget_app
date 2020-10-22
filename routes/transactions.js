ce43798b83bf6ea1d488c51545d31246a33c2235
Michael Singhurse <michaelsinghurse@gmail.com>
2020-10-22 11:49:48 -0500

    added the delete transaction route and edited some of the persistence methods comments

--- routes/transactions.js
+++ routes/transactions.js
@@ -8,6 +8,7 @@ router.use((_req, res, next) => {
   next();
 });
 
+// HTTP GET /transactions
 // respond with all the transactions for the user
 // json object: { transactions: [{}, {}, ... {}] }
 router.get("/", async (req, res, _next) => {
@@ -18,6 +19,7 @@ router.get("/", async (req, res, _next) => {
   res.json({ transactions });
 });
 
+// HTTP POST /transactions
 // Create a new transaction
 // TODO: validate and sanitize form input
 router.post("/", async (req, res, next) => {
@@ -27,6 +29,18 @@ router.post("/", async (req, res, next) => {
     next(new Error("Unable to create transaction!"));
   }
   res.redirect(303, "/");
+});
+
+// HTTP DELETE /transactions/{id}
+// Respond with 204 (No Content) on success
+router.delete("/:id", async (req, res, next) => {
+  const id = req.params.id;
+  const store = res.locals.store; 
+  const success = store.deleteTransaction(id);
+  if (!success) {
+    next(new Error("Unable to delete transaction!"));
+  }
+  res.redirect(204, "/");
 });
 
 module.exports = router;
