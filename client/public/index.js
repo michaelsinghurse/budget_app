e440a5ee026132917ed7d4bac010ae3fa4dfa93b
Michael Singhurse <michaelsinghurse@gmail.com>
2020-10-30 16:08:17 -0500

    started building out the transactions page React components

--- client/public/index.js
+++ client/public/index.js
@@ -1,3 +1,365 @@
-document.addEventListener("DOMContentLoaded", function (_event) {
-  console.log("hello world");
-});
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+// TODO: after mounting, send get requests for a list of payment source id-name
+// pairs and a list of payment category id-name pairs. use these lists to
+// populate the select boxes.
+var NewTransactionForm = function (_React$Component) {
+  _inherits(NewTransactionForm, _React$Component);
+
+  function NewTransactionForm(props) {
+    _classCallCheck(this, NewTransactionForm);
+
+    return _possibleConstructorReturn(this, (NewTransactionForm.__proto__ || Object.getPrototypeOf(NewTransactionForm)).call(this, props));
+  }
+
+  _createClass(NewTransactionForm, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "form",
+        { action: "/transactions", method: "post" },
+        React.createElement(
+          "fieldset",
+          null,
+          React.createElement(
+            "legend",
+            null,
+            "New Transaction"
+          ),
+          React.createElement(
+            "dl",
+            null,
+            React.createElement(
+              "dt",
+              null,
+              React.createElement(
+                "label",
+                { htmlFor: "date" },
+                "Date"
+              )
+            ),
+            React.createElement(
+              "dd",
+              null,
+              React.createElement("input", { type: "date", name: "date", id: "date", required: true })
+            ),
+            React.createElement(
+              "dt",
+              null,
+              React.createElement(
+                "label",
+                { htmlFor: "sourceId" },
+                "Payment Source"
+              )
+            ),
+            React.createElement(
+              "dd",
+              null,
+              React.createElement(
+                "select",
+                { name: "sourceId", id: "sourceId", required: true, defaultValue: "0" },
+                React.createElement(
+                  "option",
+                  { value: "0", disabled: true },
+                  "Choose one"
+                ),
+                React.createElement(
+                  "option",
+                  { value: "1" },
+                  "Checking"
+                ),
+                React.createElement(
+                  "option",
+                  { value: "2" },
+                  "Savings"
+                )
+              )
+            ),
+            React.createElement(
+              "dt",
+              null,
+              React.createElement(
+                "label",
+                { htmlFor: "payee" },
+                "Payee"
+              )
+            ),
+            React.createElement(
+              "dd",
+              null,
+              React.createElement("input", { type: "text", name: "payee", id: "payee", required: true })
+            ),
+            React.createElement(
+              "dt",
+              null,
+              React.createElement(
+                "label",
+                { htmlFor: "categoryId" },
+                "Category"
+              )
+            ),
+            React.createElement(
+              "dd",
+              null,
+              React.createElement(
+                "select",
+                { name: "categoryId", id: "categoryId", required: true, defaultValue: "0" },
+                React.createElement(
+                  "option",
+                  { value: "0", disabled: true },
+                  "Choose one"
+                ),
+                React.createElement(
+                  "option",
+                  { value: "1" },
+                  "Groceries"
+                ),
+                React.createElement(
+                  "option",
+                  { value: "2" },
+                  "Rent"
+                ),
+                React.createElement(
+                  "option",
+                  { value: "3" },
+                  "Electricity"
+                ),
+                React.createElement(
+                  "option",
+                  { value: "4" },
+                  "M/U Incentive"
+                ),
+                React.createElement(
+                  "option",
+                  { value: "5" },
+                  "Michael Vitamins"
+                ),
+                React.createElement(
+                  "option",
+                  { value: "6" },
+                  "Home Supplies"
+                ),
+                React.createElement(
+                  "option",
+                  { value: "7" },
+                  "Girls needs"
+                )
+              )
+            ),
+            React.createElement(
+              "dt",
+              null,
+              React.createElement(
+                "label",
+                { htmlFor: "amount" },
+                "Amount"
+              )
+            ),
+            React.createElement(
+              "dd",
+              null,
+              React.createElement("input", { type: "number", name: "amount", id: "amount", step: "0.01", required: true })
+            ),
+            React.createElement(
+              "dt",
+              null,
+              React.createElement(
+                "label",
+                { htmlFor: "notes" },
+                "Notes"
+              )
+            ),
+            React.createElement(
+              "dd",
+              null,
+              React.createElement("input", { type: "text", name: "notes", id: "notes" })
+            )
+          ),
+          React.createElement("input", { type: "submit", value: "Add" })
+        )
+      );
+    }
+  }]);
+
+  return NewTransactionForm;
+}(React.Component);
+
+// TODO: Handle split transactions.
+
+
+function TransactionTableRow(props) {
+  var transaction = props.transaction;
+  var formatDate = function formatDate(dateString) {
+    return new Date(dateString).toLocaleDateString(options);
+  };
+  return React.createElement(
+    "tr",
+    null,
+    React.createElement(
+      "td",
+      null,
+      formatDate(transactions.date)
+    ),
+    React.createElement(
+      "td",
+      null,
+      transaction.source
+    ),
+    React.createElement(
+      "td",
+      null,
+      transaction.payee
+    ),
+    React.createElement(
+      "td",
+      null,
+      transaction.categories[0].category
+    ),
+    React.createElement(
+      "td",
+      null,
+      transaction.categories[0].amount
+    ),
+    React.createElement(
+      "td",
+      null,
+      transaction.notes
+    )
+  );
+}
+
+var TransactionsTable = function (_React$Component2) {
+  _inherits(TransactionsTable, _React$Component2);
+
+  function TransactionsTable(props) {
+    _classCallCheck(this, TransactionsTable);
+
+    return _possibleConstructorReturn(this, (TransactionsTable.__proto__ || Object.getPrototypeOf(TransactionsTable)).call(this, props));
+  }
+
+  _createClass(TransactionsTable, [{
+    key: "render",
+    value: function render() {
+      var transactions = this.props.transactions || [];
+      var tableRows = transactions.map(function (transaction) {
+        return React.createElement(TransactionTableRow, {
+          key: transaction.id,
+          transaction: transaction });
+      });
+
+      return React.createElement(
+        "table",
+        null,
+        React.createElement(
+          "thead",
+          null,
+          React.createElement(
+            "tr",
+            null,
+            React.createElement(
+              "th",
+              null,
+              "Date"
+            ),
+            React.createElement(
+              "th",
+              null,
+              "Source"
+            ),
+            React.createElement(
+              "th",
+              null,
+              "Payee"
+            ),
+            React.createElement(
+              "th",
+              null,
+              "Category"
+            ),
+            React.createElement(
+              "th",
+              null,
+              "Amount"
+            ),
+            React.createElement(
+              "th",
+              null,
+              "Notes"
+            )
+          )
+        ),
+        React.createElement(
+          "tbody",
+          null,
+          tableRows
+        )
+      );
+    }
+  }]);
+
+  return TransactionsTable;
+}(React.Component);
+
+// TODO: pass an on submit handler to the NewTransactionForm. the handler should
+// submit the new transaction to the server and handle the response. on success,
+// either add the new transaction to the list of transactions maintained in
+// state or send a request for entirely new set of transactions. pass those
+// returned transactions to TransactionsTable
+// TODO: after mounting, send a request to the server for a list of
+// transactions. pass those transactions to TransactionsTable
+
+
+var Transactions = function (_React$Component3) {
+  _inherits(Transactions, _React$Component3);
+
+  function Transactions(props) {
+    _classCallCheck(this, Transactions);
+
+    var _this3 = _possibleConstructorReturn(this, (Transactions.__proto__ || Object.getPrototypeOf(Transactions)).call(this, props));
+
+    _this3.state = {
+      transactions: null
+    };
+    return _this3;
+  }
+
+  _createClass(Transactions, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var _this4 = this;
+
+      fetch("/transactions").then(function (response) {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      }).then(function (data) {
+        _this4.setState({
+          transactions: data.transactions
+        });
+      }).catch(function (error) {
+        console.error("Problem with fetch operations:", error);
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(NewTransactionForm, null),
+        React.createElement(TransactionsTable, { transactions: this.state.transactions })
+      );
+    }
+  }]);
+
+  return Transactions;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Transactions, null), document.getElementById("react-root"));
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,151 @@
-document.addEventListener("DOMContentLoaded", _event => {
-  console.log("hello world");
-});
+// TODO: after mounting, send get requests for a list of payment source id-name
+// pairs and a list of payment category id-name pairs. use these lists to
+// populate the select boxes.
+class NewTransactionForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <form action="/transactions" method="post">
+        <fieldset>
+          <legend>New Transaction</legend>
+          <dl>
+            <dt><label htmlFor="date">Date</label></dt>
+            <dd><input type="date" name="date" id="date" required/></dd>
+            <dt><label htmlFor="sourceId">Payment Source</label></dt>
+            <dd>
+              <select name="sourceId" id="sourceId" required defaultValue="0">
+                <option value="0" disabled>Choose one</option>
+                <option value="1">Checking</option>
+                <option value="2">Savings</option>
+              </select>
+            </dd>
+            <dt><label htmlFor="payee">Payee</label></dt>
+            <dd><input type="text" name="payee" id="payee" required/></dd>
+            <dt><label htmlFor="categoryId">Category</label></dt>
+            <dd>
+              <select name="categoryId" id="categoryId" required defaultValue="0" >
+                <option value="0" disabled >Choose one</option>
+                <option value="1">Groceries</option>
+                <option value="2">Rent</option>
+                <option value="3">Electricity</option>
+                <option value="4">M/U Incentive</option>
+                <option value="5">Michael Vitamins</option>
+                <option value="6">Home Supplies</option>
+                <option value="7">Girls needs</option>
+              </select>
+            </dd>
+            <dt><label htmlFor="amount">Amount</label></dt>
+            <dd>
+              <input type="number" name="amount" id="amount" step="0.01" required/>
+            </dd>
+            <dt><label htmlFor="notes">Notes</label></dt>
+            <dd><input type="text" name="notes" id="notes" /></dd>
+          </dl>
+          <input type="submit" value="Add" />
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+// TODO: Handle split transactions.
+function TransactionTableRow(props) {
+  const transaction = props.transaction;
+  const formatDate = dateString => {
+    return (new Date(dateString)).toLocaleDateString(options);
+  };
+  return (
+    <tr>
+      <td>{formatDate(transactions.date)}</td>
+      <td>{transaction.source}</td>
+      <td>{transaction.payee}</td>
+      <td>{transaction.categories[0].category}</td>
+      <td>{transaction.categories[0].amount}</td>
+      <td>{transaction.notes}</td>
+    </tr>
+  );
+}
+
+class TransactionsTable extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const transactions = this.props.transactions || [];
+    const tableRows = transactions.map(transaction => {
+      return <TransactionTableRow 
+                key={transaction.id} 
+                transaction={transaction} />;
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Source</th>
+            <th>Payee</th>
+            <th>Category</th>
+            <th>Amount</th>
+            <th>Notes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableRows}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+// TODO: pass an on submit handler to the NewTransactionForm. the handler should
+// submit the new transaction to the server and handle the response. on success,
+// either add the new transaction to the list of transactions maintained in
+// state or send a request for entirely new set of transactions. pass those
+// returned transactions to TransactionsTable
+// TODO: after mounting, send a request to the server for a list of
+// transactions. pass those transactions to TransactionsTable
+class Transactions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transactions: null,
+    };
+  }
+  
+  componentDidMount() {
+    fetch("/transactions")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          transactions: data.transactions,
+        });
+      })
+      .catch(error => {
+        console.error("Problem with fetch operations:", error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <NewTransactionForm />
+        <TransactionsTable transactions={this.state.transactions} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Transactions />,
+  document.getElementById("react-root")
+);
